84a94de963ad0cc85f1e5bc79d342897a2e57a0e
Hannes Breul <hannes.breul@infarm.com>
2023-10-01 17:45:51 +0200

    chore(format): Format all remaining files

--- app/notes/tests.py
+++ app/notes/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+
+"""
+TODO - test specs:
+
+fixtures: two users
+
+- user can create notes
+- user can list notes
+- user can tag notes
+- tags get created if nonexistent
+- user can edit notes (change content, change tags)
+- user can delete their own notes
+- user can filter notes by tag
+- user sees only their own notes
+- user can not access other user's note
+- user can not change ownership of note
+- user can not edit or delete other user's note
+- user can search notes by keyword
+"""
